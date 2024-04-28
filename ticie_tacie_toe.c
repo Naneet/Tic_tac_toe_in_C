ca635e7cbd310e0c93233623c9bc39fce2b70d08
Aryan Nishad <157164036+aryannishad-86@users.noreply.github.com>
2024-04-28 14:03:54 +0530

    Update ticie_tacie_toe.c
    2nd time doing this thing xD

--- ticie_tacie_toe.c
+++ ticie_tacie_toe.c
@@ -1,108 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void intro()
-{
-    int example[] = { 0 , 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    printf("Welcome to the game of tic tac toe \n");
-    printf("Following are the numbers corresponding place on the board \n");
-    printf("%d | %d | %d \n", example[7], example[8], example[9]);
-    printf("--|---|-- \n");
-    printf("%d | %d | %d \n", example[4], example[5], example[6]);
-    printf("--|---|-- \n");
-    printf("%d | %d | %d \n", example[1], example[2], example[3]);
-    printf("You may enter the number to place 'X' or 'O' in your turn \n");
+char moves[10] = {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+char played[10];
+char allowed_moves[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char player1, player2;
+
+void intro() {
+    printf("Welcome to the game of tic tac toe\n");
+    printf("Following are the numbers corresponding place on the board\n");
+    printf("7|8|9\n");
+    printf("4|5|6\n");
+    printf("1|2|3\n");
+    printf("You may enter the number to place 'X' or 'O' in your turn\n");
 }
 
-void display(char moves[10])
-{
-    printf("%c | %c | %c\n", moves[7], moves[8], moves[9]);
-    printf("--|---|-- \n");
-    printf("%c | %c | %c\n", moves[4], moves[5], moves[6]);
-    printf("--|---|-- \n");
-    printf("%c | %c | %c\n", moves[1], moves[2], moves[3]);
+void display() {
+    printf("%c|%c|%c\n", moves[7], moves[8], moves[9]);
+    printf("%c|%c|%c\n", moves[4], moves[5], moves[6]);
+    printf("%c|%c|%c\n", moves[1], moves[2], moves[3]);
 }
 
-
-int win_condition(char moves[], char player1, char player2, int played[], int next_play, int z)
-{
-    int n = 1;
-    while (n < 8)
-    {
-        if (moves[n] == moves[n + 1] == moves[n + 2] == player1)
-        {
-            printf("Player 1 won (horizontal) \n");
-            printf("Sometimes you are never meant to win player 2!! \n");
-            return 0;
-        }
-        else if (moves[n] == moves[n + 1] == moves[n + 2] == player2)
-        {
-            printf("Player 2 won (horizontal) \n");
-            printf("Sometimes you are never meant to win player 1!! \n");
-            return 0;
-        }
-        n = n + 3;
-    }
-
-    n = 1;
-    while (n < 4)
-    {
-        if (moves[n] == moves[n + 3] == moves[n + 6] == player1)
-        {
-            printf("Player 1 won (vertical) \n");
-            printf("Sometimes you are never meant to win player 2!! \n");
-            return 0;
-        }
-        else if (moves[n] == moves[n + 3] == moves[n + 6] == player2)
-        {
-            printf("Player 2 won (vertical) \n");
-            printf("Sometimes you are never meant to win player 1!! \n");
-            return 0;
-        }
-        n = n + 1;
-
-
-    }
-
-    n = 1;
-
-    if (moves[n] == moves[n + 4] == moves[n + 8] == player1)
-    {
-        printf("Player 1 won (diagonal)\n");
-        printf("Better luck next time Player 2!\n");
-        return 0;
-    }
-
-    else if (moves[n] == moves[n + 4] == moves[n + 8] == player2)
-    {
-        printf("Player 2 won (diagonal)\n");
-        printf("Better luck next time Player 1!\n");
-        return 0;
-    }
-
-    else if (moves[n + 2] == moves[n + 4] == moves[n + 6] == player1)
-    {
-        printf("Player 1 won (diagonal)\n");
-        printf("Player 2 you can do better!\n");
-        return 0;
-    }
-
-    else if (moves[n + 2] == moves[n + 4] == moves[n + 6] == player2)
-    {
-        printf("Player 2 won (diagonal)\n");
-        printf("Player 1 you can do better!\n");
-        return 0;
-    }
-
-    else if (z == 9)
-    {
-        printf("It's a tie!!\n");
-        return 0;
-    }
-
-}
-
-void select_player1(char moves[10], int played[], int z) {
-    char player1, player2;
+void select_player1() {
     printf("Player 1 ('X' or 'O'):");
     scanf(" %c", &player1);
     while (player1 != 'X' && player1 != 'O') {
@@ -117,14 +37,11 @@ void select_player1(char moves[10], int played[], int z) {
     }
     printf("Player 1 is %c\n", player1);
     printf("Player 2 is %c\n", player2);
-    display(moves[10]);
-    
+    display();
 }
 
-void play1(int played[], char player1, char player2, char moves[10], int z) {
+void play1() {
     int move_played;
-    int next_play = 2;
-    z += 1;
     printf("Player 1's Turn (1-9):");
     scanf("%d", &move_played);
     while (move_played < 1 || move_played > 9 || moves[move_played] != ' ') {
@@ -133,14 +50,11 @@ void play1(int played[], char player1, char player2, char moves[10], int z) {
         scanf("%d", &move_played);
     }
     moves[move_played] = player1;
-    display(moves[10]);
-    win_condition(moves[10], player1, player2, played[], next_play, z);
+    display();
 }
 
-void play2(int played[], char player1, char player2, char moves[10], int z) {
+void play2() {
     int move_played;
-    int next_play = 1;
-    z += 1;
     printf("Player 2's Turn (1-9):");
     scanf("%d", &move_played);
     while (move_played < 1 || move_played > 9 || moves[move_played] != ' ') {
@@ -149,43 +63,88 @@ void play2(int played[], char player1, char player2, char moves[10], int z) {
         scanf("%d", &move_played);
     }
     moves[move_played] = player2;
-    display(moves[10]);
-    played[z] = move_played;
-    
+    display();
 }
 
+void win_condition() {
+    int n = 1;
+    while (n < 8) {
+        if (moves[n] == moves[n+1] && moves[n+1] == moves[n+2] && moves[n+2] == player1) {
+            printf("Player 1 won (horizontal)\n");
+            printf("Sometimes you are never meant to win player 2!!\n");
+            return;
+        } else if (moves[n] == moves[n+1] && moves[n+1] == moves[n+2] && moves[n+2] == player2) {
+            printf("Player 2 won (horizontal)\n");
+            printf("👀👀\n");
+            return;
+        }
+        n = n + 3;
+    }
 
+    n = 1;
+    while (n < 4) {
+        if (moves[n] == moves[n+3] && moves[n+3] == moves[n+6] && moves[n+6] == player1) {
+            printf("Player 1 won (vertical)\n");
+            printf("Sometimes you are never meant to win player 2!!\n");
+            return;
+        } else if (moves[n] == moves[n+3] && moves[n+3] == moves[n+6] && moves[n+6] == player2) {
+            printf("Player 2 won (vertical)\n");
+            return;
+        }
+        n = n + 1;
+    }
+
+    n = 1;
+    if (moves[n] == moves[n+4] && moves[n+4] == moves[n+8] && moves[n+8] == player1) {
+        printf("Player 1 won (diagonal)\n");
+        printf("👀👀\n");
+    } else if (moves[n] == moves[n+4] && moves[n+4] == moves[n+8] && moves[n+8] == player2) {
+        printf("Player 2 won (diagonal)\n");
+    } else if (moves[n+2] == moves[n+4] && moves[n+4] == moves[n+6] && moves[n+6] == player1) {
+        printf("Player 1 won (diagonal)\n");
+        printf("Sometimes you are never meant to win player 2!!\n");
+    } else if (moves[n+2] == moves[n+4] && moves[n+4] == moves[n+6] && moves[n+6] == player2) {
+        printf("Player 2 won (diagonal)\n");
+    } else {
+        int i;
+        for (i = 1; i < 10; i++) {
+            if (moves[i] == ' ') {
+                return;
+            }
+        }
+        printf("It's a tie!!\n");
+        printf("👀👀\n");
+    }
+}
 
 char play_again() {
-    char again = "m";
+    char again[2];
     printf("Play again? (Y or N):");
-    scanf("%c", &again);
-    while (again != "Y" && again != "y" && again != "N" && again != "n") {
+    scanf("%s", again);
+    while (strcmp(again, "Y") != 0 && strcmp(again, "N") != 0) {
         printf("Oops! Seems like you entered an invalid option\n");
         printf("Play again? (Y or N):");
-        scanf("%c", &again);
+        scanf("%s", again);
     }
-    if (again == "N" || again == "n") {
+    if (strcmp(again, "N") == 0) {
         printf("Thanks for playing :)\n");
     }
-    return again;
+    return again[0];
 }
-
-void intro();
-void select_player1(char moves[], char played[]);
-char play_again();
 
 int main() {
     char again = 'Y';
-    while z=0(again == 'Y') {
-        char Player_Options[2] = {'X', 'O'};  // A list for cross-checking inputs
-
-        char moves[10] = "#         ";  // To record all the moves of the players
-
-        char played[10] = "";  // Positions that have already been played
-
+    while (again == 'Y') {
+        memset(moves, ' ', sizeof(moves));
+        moves[0] = '#';
         intro();
-        select_player1(moves, played);
+        select_player1();
+        while (1) {
+            play1();
+            win_condition();
+            play2();
+            win_condition();
+        }
         again = play_again();
     }
     return 0;
